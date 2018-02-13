9d653f7403e444a9b64297a866010bd4506d5c56
cbravoa@gmail.com <cbravoa@gmail.com>
2018-02-13 23:01:52 +0000

    image rekognition with AWS

---
+++ image-and-rekog-button.py
@@ -0,0 +1,64 @@
+import boto3
+import picamera
+import time
+import RPi.GPIO as GPIO
+
+print('Initializing button pin')
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+BUCKET = "raspibucket2"
+REGION = "eu-west-1"
+
+def take_picture():
+   print("Attempting to take the picture. Smile! :)")
+   ts = int(time.time())
+   filename = 'image-'+ str(ts) + '.jpg'
+   camera = picamera.PiCamera()
+   camera.capture(filename)
+   print("Picture taken")
+   camera.close()
+   return filename
+
+def upload_picture(filename):
+   print("Attempting to upload the picture")
+   s3_client = boto3.client('s3', REGION)
+   s3_client.upload_file(filename, BUCKET, filename)
+   print("Picture uploaded")
+
+def detect_labels(bucket, key, max_labels=10, min_confidence=90, region=REGION):
+        print("Attempting to call the Rekognition service :O")
+	rekognition = boto3.client("rekognition", region)
+	response = rekognition.detect_labels(
+		Image={
+			"S3Object": {
+				"Bucket": bucket,
+				"Name": key,
+			}
+		},
+		MaxLabels=max_labels,
+		MinConfidence=min_confidence,
+	)
+        print("Done!")
+	return response['Labels']
+
+def show_labels(labels):
+   for label in labels:
+     print "{Name} - {Confidence}%".format(**label)
+
+
+def take_and_rekog():
+  filename = take_picture()
+  upload_picture(filename)
+  res = detect_labels(BUCKET, filename, 10, 80)
+  show_labels(res)
+
+print('Starting cycle')
+while True:
+  input_state = GPIO.input(18)
+  if input_state == False:
+     print('Button pressed! Taking picture')
+     take_and_rekog()
+     time.sleep(0.2)
+     print('Ready!')
